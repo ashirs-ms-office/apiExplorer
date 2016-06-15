fcf00666eb74097bcde1d38163bf5c6447a27d8e
Elizabeth Dinella <t-eldine@microsoft.com>
2016-06-15 10:25:26 -0700

    disabled tab in editor

--- src/api-explorer-jseditor.js
+++ src/api-explorer-jseditor.js
@@ -2,5 +2,7 @@ function initializeJsonEditor($scope) {
     jsonEditor = ace.edit("jsonEditor");
     jsonEditor.getSession().setMode("ace/mode/javascript");
     jsonEditor.setShowPrintMargin(false);
+    //accessibility - keyboard dependant users must be able to "tab out" of session
+    jsonEditor.commands.bindKey("Tab", null);
     $scope.jsonEditor = jsonEditor;
 }
