2e1692cb89bfc193f5f952c55be53d34f0a32e5b
elizabethdinella <elizabeth.a.dinella@gmail.com>
2016-06-19 18:58:20 -0400

    new tests

--- spec/api-explorer-ctrl-spec.js
+++ spec/api-explorer-ctrl-spec.js
@@ -16,7 +16,7 @@ describe("DropdownCtrl", function(){
               controller = $controller('DropdownCtrl', { $scope: $scope });
             });
         
-          it('sets the selectedOption and the parent selectedOption to the input', function() {
+          it('sets the parent selectedOption to the input', function() {
               
               $scope.OnItemClick("GET");
               expect($scope.$parent.selectedOptions).toEqual("GET");
@@ -45,6 +45,67 @@ describe("DropdownCtrl", function(){
            expect($scope.$parent.showJsonEditor).toBe(false);
            
         });
+    });
+
+});
+
+describe("VersionCtrl", function(){
+    beforeEach(angular.mock.module('ApiExplorer'));
+    var $controller;
+    
+    beforeEach(inject(function(_$controller_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+    
+    describe("$scope.OnItemClick", function(){
+        
+           var $scope, controller;
+
+            beforeEach(function() {
+              $scope = {};
+              controller = $controller('VersionCtrl', { $scope: $scope });
+            });
+        
+          it('sets the parent selectedVersion to the input', function() {
+              
+              $scope.OnItemClick("beta");
+              expect($scope.$parent.selectedVersion).toEqual("beta");
+        
+              $scope.OnItemClick("v1.0");
+              expect($scope.$parent.selectedOptions).toEqual("v1.0");
+
+          });
+        
+    });
+
+});
+
+describe("datalistCtrl", function(){
+    beforeEach(angular.mock.module('ApiExplorer'));
+    var $controller;
+    
+    beforeEach(inject(function(_$controller_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+    
+    describe("$scope.$parent.$on('clearUrls)", function(){
+        
+           var $scope, controller;
+
+            beforeEach(function() {
+              $scope = {};
+              controller = $controller('datalistCtrl', { $scope: $scope });
+            });
+        
+          it('sets the urlOptions array to empty', function() {
+              
+              $scope.$emit("clearUrls");
+              expect($scope.urlOptions).toEqual([]);
+
+          });
+        
     });
 
 });
