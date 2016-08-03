be5b52b72466432b5c9a452d765fd356cb771d01
Elizabeth Dinella <t-eldine@microsoft.com>
2016-08-03 14:57:44 -0700

    Revert "added files.readWrite to user scopes"
    This reverts commit cd53fbeea1b3c6f3e7a7db92163142cdb01b6242.

--- src/api-explorer-init.js
+++ src/api-explorer-init.js
@@ -33,7 +33,7 @@ angular.module('ApiExplorer', ['ngRoute', 'AdalAngular', 'ngAnimate', 'ui.bootst
                 clientId: '41359d1a-a069-4a6b-aaf1-b398c18b6c16', //'2e8459fe-87ef-4286-af70-f33a307563aa',
                 endpoints: {
                     "https://graph.microsoft.com" :{
-                    scope:["calendars.readWrite contacts.readWrite files.readWrite files.readAll user.readWrite mail.readWrite mail.send sites.read.all tasks.readWrite people.read notes.readWrite.all"] 
+                    scope:["calendars.readWrite contacts.readWrite files.read.all user.readWrite mail.readWrite mail.send sites.read.all tasks.readWrite people.read notes.readWrite.all"] 
                   } 
                 
                 }, 
